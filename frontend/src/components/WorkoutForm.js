461bab314958c5eaa56c3fd69d921df5a45f273d
PrajwalCC <b220035@iiit-bh.ac.in>
2023-04-01 17:50:59 +0530

    fetch routing

--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -22,7 +22,7 @@ const WorkoutForm = () => {
 
     const workout = {title, load, reps}
 
-    const response = await fetch(backendUrl+'/api/workouts', {
+    const response = await fetch(`${backendUrl}/api/workouts/`, {
       method: 'POST',
       body: JSON.stringify(workout),
       headers: {
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -12,7 +12,7 @@ const Home = () => {
 
   useEffect(() => {
     const fetchWorkouts = async () => {
-      const response = await fetch(backendUrl+'/api/workouts', {
+      const response = await fetch(`${backendUrl}/api/workouts`, {
         headers: {'Authorization': `Bearer ${user.token}`},
       })
       const json = await response.json()
