c960cb488545a60bfb97484e7a578010edf03b9c
PrajwalCC <b220035@iiit-bh.ac.in>
2023-08-15 14:52:53 +0530

    updating cors

--- backend/server.js
+++ backend/server.js
@@ -33,12 +33,6 @@ app.options(
     credentials: true,
   })
 );
-// app.use((req, res, next) => {
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-//   next();
-// });
 
 // routes
 app.use('/api/workouts', workoutRoutes)
